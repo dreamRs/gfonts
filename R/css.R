5d9bf74f59ceff5e18328e04eee411b7a070c5ea
pvictor <perrier.victor@gmail.com>
2019-11-07 16:17:43 +0100

    glue css template

---
+++ R/css.R
@@ -0,0 +1,22 @@
+
+
+glue_css <- function(font_info, path = "../fonts/") {
+  template <- readLines(system.file("assets/templates/css_best.txt", package = "gfonts"))
+  template <- paste(template, collapse = "\n")
+  res <- lapply(
+    X = seq_len(nrow(font_info$variants)),
+    FUN = function(i) {
+      glue::glue(
+        template,
+        variant = font_info$variants[i, ],
+        fontItem = font_info,
+        folderPrefix = path
+      )
+    }
+  )
+  pasten <- function(...)
+    paste(..., sep = "\n")
+  Reduce(f = pasten, x = res)
+}
+
+
