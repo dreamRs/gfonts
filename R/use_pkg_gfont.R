9a8f3a5c250f595acac9ba5b897b58e637a1a48d
pvictor <perrier.victor@gmail.com>
2020-01-12 11:31:08 +0100

    reorganize files

---
+++ R/use_pkg_gfont.R
@@ -0,0 +1,54 @@
+
+#' @title Use a Google Font included in {gfonts}
+#'
+#' @description For convenience, some fonts are included in the package,
+#'  you can use them without having to download them, but only few variants are available.
+#'
+#' @param font Name of the font to use, possible choices are: \code{"roboto"},
+#'  \code{"open-sans"}, \code{"lato"}, \code{"montserrat"}, \code{"alegreya"},
+#'  \code{"nunito-sans"}, \code{"baloo"}, \code{"happy-monkey"}, \code{"henny-penny"}.
+#' @param selector CSS selector for which to use the font,
+#'  usually an HTML tag, default to \code{"body"} (all document).
+#'
+#' @return an \code{htmlDependency}.
+#' @export
+#'
+#' @importFrom htmltools htmlDependency attachDependencies tags
+#' @importFrom glue glue
+#'
+#' @example examples/ex-use_pkg_gfont.R
+use_pkg_gfont <- function(font = c("roboto", "open-sans", "lato", "montserrat",
+                                   "alegreya", "nunito-sans", "baloo", "happy-monkey",
+                                   "henny-penny"),
+                          selector = "body") {
+
+  font <- match.arg(arg = font)
+
+  selector <- paste(selector, collapse = ", ")
+
+  css <- switch(
+    font,
+    "roboto" = "font-family: 'Roboto', sans-serif;",
+    "open-sans" = "font-family: 'Open Sans', sans-serif;",
+    "lato" = "font-family: 'Lato', sans-serif;",
+    "montserrat" = "font-family: 'Montserrat', sans-serif;",
+    "alegreya" = "font-family: 'Alegreya', serif;",
+    "nunito-sans" = "font-family: 'Nunito Sans', sans-serif;",
+    "baloo" = "font-family: 'Baloo', cursive;",
+    "happy-monkey" = "font-family: 'Happy Monkey', cursive;",
+    "henny-penny" = "font-family: 'Henny Penny', cursive;"
+  )
+  css <- glue::glue("{selector} {{{css}}}", selector = selector, css = css)
+  css <- paste(css, collapse = "\n")
+
+  attachDependencies(
+    x = tags$style(css),
+    value = htmlDependency(
+      name = font,
+      version = "0.1.0",
+      src = list(href = "gfonts", file = "assets"),
+      package = "gfonts",
+      stylesheet = sprintf("css/%s.css", font)
+    )
+  )
+}
