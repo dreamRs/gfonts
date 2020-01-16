e2aa341a13f6d9866696e369570dd346896aaea6
pvictor <perrier.victor@gmail.com>
2020-01-16 12:29:47 +0100

    added package doc

---
+++ R/gfonts-package.R
@@ -0,0 +1,10 @@
+
+#' @title Use Google fonts offline
+#'
+#' @description Download Google fonts and generate CSS to use in {rmarkdown} documents and
+#' {shiny} applications. Some popular fonts are included and ready to use.
+#'
+#' @name gfonts
+#' @docType package
+#' @author Victor Perrier (@@dreamRs_fr)
+NULL
